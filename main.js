96ca39181ab25fb511698d7d9c45c7a7776f4bd3
LoiNguyenn1 <nguyenloi9072@gmail.com>
2026-03-07 17:50:04 +0700

    Up

---
+++ main.js
@@ -0,0 +1,11 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+function openPopup(){
+    document.getElementById("popup").style.display="flex";
+}
+
+function closePopup(){
+    document.getElementById("popup").style.display="none";
+}
+audioElement.play();
